38d55224a818f5b96bddcc641b558f70a48e72da
Hassan Panahi <hassan.panahi@samim.net>
2023-09-05 10:46:18 +0330

    Relase to Co

--- src/main.cc
+++ src/main.cc
@@ -8,34 +8,29 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-    //    if (argc != 3) {
-    //        std::cerr << "Usage: " << argv[0] << " <input_file_address> <output_file_address>" << std::endl;
-    //        return 1;
-    //    }
+    if (argc != 3) {
+        std::cerr << "Usage: " << argv[0] << " <input_file_address> <output_file_address>" << std::endl;
+        return 1;
+    }
 
     int ret = 0;
     try {
-        for (uint32_t i= 1 ; i < 8; i++) {
+        const char* input_file_address = argv[1];
+        const char* output_file_address = argv[2];
 
-            std::string inputs = "/home/hp/Videos/knight-tour-max-point/inputs/input"+ std::to_string(i)+ ".txt";
-            std::string output = "/home/hp/Videos/knight-tour-max-point/inputs/output"+ std::to_string(i)+ ".txt";
+        algorithms::ChessTableStructure table_structure;
+        table_structure.table_row = Chess_Table_Row_Count;
+        table_structure.table_col = Chess_Table_Col_Count;
+        table_structure.move_strategy = algorithms::ChessTableMoveStrategyAbstract::get_positions(algorithms::ChessPieceType::Knight);
 
-            const char* input_file_address = inputs.c_str(); //argv[1];
-            const char* output_file_address = output.c_str();//argv[2];
+        algorithms::ChessTableIOHandler ChessTableIOHandler(input_file_address, output_file_address);
+        table_structure.input_chess_pieces = ChessTableIOHandler.get_table_chess_info();
 
-            algorithms::ChessTableStructure table_structure;
-            table_structure.table_row = Chess_Table_Row_Count;
-            table_structure.table_col = Chess_Table_Col_Count;
-            table_structure.move_strategy = algorithms::ChessTableMoveStrategyAbstract::get_positions(algorithms::ChessPieceType::Knight);
+        algorithms::ChessTable table(table_structure);
+        auto result = table.solve_table();
 
-            algorithms::ChessTableIOHandler ChessTableIOHandler(input_file_address, output_file_address);
-            table_structure.input_chess_pieces = ChessTableIOHandler.get_table_chess_info();
+        ChessTableIOHandler.report_chess_result(result);
 
-            algorithms::ChessTable table(table_structure);
-            auto result = table.solve_table();
-
-            ChessTableIOHandler.report_chess_result(result);
-        }
     } catch (std::invalid_argument& excep) {
         ret = -1;
         std::cout << "error: " << excep.what() << std::endl;
